0a896aa8cc27047359f897e5e91c802a8938394f
PunGrumpy <108584943+PunGrumpy@users.noreply.github.com>
2024-06-29 15:17:55 +0700

    feat(metadata): add og and twitter

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,6 +26,34 @@ export const metadata: Metadata = {
     icon: '/favicon.ico',
     shortcut: '/logo.png',
     apple: '/apple-touch-icon.png'
+  },
+  robots: 'follow, index',
+  openGraph: {
+    title: 'Curriculum Vitae',
+    description: 'A minimalist CV page',
+    type: 'website',
+    images: [
+      {
+        url: '/og-image.png',
+        width: 1200,
+        height: 630,
+        alt: 'Curriculum Vitae'
+      }
+    ]
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: 'Curriculum Vitae',
+    description: 'A minimalist CV page',
+    images: [
+      {
+        url: '/twitter-card.png',
+        width: 1500,
+        height: 500,
+        alt: 'Curriculum Vitae'
+      }
+    ],
+    site: '@cv'
   }
 }
 
