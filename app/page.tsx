ef5143d4dcc8c5371484dc122a1dd7a13f383210
PunGrumpy <108584943+PunGrumpy@users.noreply.github.com>
2024-06-30 14:10:49 +0700

    feat(server): move to use `force-dynamic`

--- app/page.tsx
+++ app/page.tsx
@@ -8,45 +8,11 @@ import EducationSection from '../components/section/education'
 import SkillSection from '../components/section/skill'
 import ProjectSection from '../components/section/project'
 import Link from 'next/link'
-import prisma from '@/lib/prisma'
 import AlertSection from '@/components/section/alert'
+import { fetchResume } from './actions'
 
 export default async function Page() {
-  const resume = await prisma.resume.findUnique({
-    where: { id: 1, initials: 'NK' },
-    include: {
-      contact: {
-        select: {
-          email: true,
-          social: { select: { name: true, url: true, icon: true } }
-        }
-      },
-      work: {
-        select: {
-          id: true,
-          company: true,
-          link: true,
-          badge: true,
-          startYear: true,
-          endYear: true,
-          title: true,
-          description: true
-        }
-      },
-      education: {
-        select: { school: true, degree: true, startYear: true, endYear: true }
-      },
-      skills: { select: { id: true, name: true } },
-      projects: {
-        select: {
-          title: true,
-          techStack: true,
-          description: true,
-          link: true
-        }
-      }
-    }
-  })
+  const resume = await fetchResume()
 
   if (!resume) {
     return (
