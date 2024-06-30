a0a61b47c2eeaba5167c8abcf94dbf239ab7a04e
PunGrumpy <108584943+PunGrumpy@users.noreply.github.com>
2024-06-30 14:31:30 +0700

    docs(metadata): if not have `VERCEL_URL` set undefined

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,7 +17,7 @@ export const viewport = {
 export const metadata: Metadata = {
   metadataBase: process.env.VERCEL_URL
     ? new URL(`https://${process.env.VERCEL_URL}`)
-    : new URL('http://localhost:3000'),
+    : undefined,
   title: {
     default: 'Curriculum Vitae',
     template: `%s | Curriculum Vitae`
