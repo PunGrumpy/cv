f5e57621019d2001e75c67d147374545ce10d107
PunGrumpy <108584943+PunGrumpy@users.noreply.github.com>
2025-02-25 08:51:57 +0700

    refactor(section): update import path and enhance className for print styles

--- components/ui/section.tsx
+++ components/ui/section.tsx
@@ -1,13 +1,13 @@
 import React from 'react'
 
-import { cn } from '../../lib/utils'
+import { cn } from '@/lib/utils'
 
-export interface BadgeProps extends React.HTMLAttributes<HTMLDivElement> {}
+export type BadgeProps = React.HTMLAttributes<HTMLDivElement>
 
 export function Section({ className, ...props }: BadgeProps) {
   return (
     <section
-      className={cn('flex min-h-0 flex-col gap-y-3', className)}
+      className={cn('flex min-h-0 flex-col gap-y-3 print:gap-y-1', className)}
       {...props}
     />
   )
