1c49da2630f25966f276c789d9247b35eab6bcc4
PunGrumpy <108584943+PunGrumpy@users.noreply.github.com>
2025-02-25 08:47:48 +0700

    refactor(prisma): optimize PrismaClient initialization for better environment handling

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,11 +1,9 @@
 import { PrismaClient } from '@prisma/client'
 
-declare global {
-  var prisma: PrismaClient | undefined
-}
+const prisma = new PrismaClient()
 
-const prisma = global.prisma || new PrismaClient()
+const globalForPrisma = global as unknown as { prisma: typeof prisma }
 
-if (process.env.NODE_ENV === 'development') global.prisma = prisma
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
 
 export default prisma
